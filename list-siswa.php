ad4b40d53e74d54323df3100bc2ba694d996278e
Hanifcahyo <hanifcahyoprasetyo95@gmail.com>
2020-12-06 11:46:58 +0700

    design

--- list-siswa.php
+++ list-siswa.php
@@ -2,56 +2,92 @@
 
 <!DOCTYPE html>
 <html>
-<head>
+    <head>
     <title>Pendaftaran Siswa Baru | SMK RUS</title>
-</head>
+        <link rel="stylesheet" href="style-list-siswa.css">
+    </head>
 
-<body>
-    <header>
-        <h3>Siswa yang sudah mendaftar</h3>
-    </header>
+    <body>
 
-    <nav>
-        <a href="form-daftar.php">[+] Tambah Baru</a>
-    </nav>
+        <div id="container">
+            <div id="header">
+                <img src="logo.png">
+                <h1>SMK Raden Umar Said</h1>
+                <p>Jalan Sukun Raya 9, Besito, Gebog, Kudus</p>
+            </div>
+        </div>
 
-    <br>
+    
+    
+        <div id="container2">
+            <div id="body">
+                <header>
+                    <h3>Siswa yang sudah mendaftar</h3>
+                 </header>
 
-    <table border="1">
-    <thead>
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Jenis Kelamin</th>
-            <th>Agama</th>
-            <th>Sekolah Asal</th>
-        </tr>
-    </thead>
-    <tbody>
+                <nav>
+                    <a href="form-daftar.php">[+] Tambah Baru</a>
+                </nav>
 
-        <?php
-        $sql = "SELECT * FROM calon_siswa";
-        $query = mysqli_query($db, $sql);
+                <br>
+                <center>    
+                <table border="1">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Alamat</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Agama</th>
+                        <th>Sekolah Asal</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                        <?php
+                        $sql = "SELECT * FROM calon_siswa";
+                        $query = mysqli_query($db, $sql);
 
-        while($siswa = mysqli_fetch_array($query)){
-            echo "<tr>";
+                    while($siswa = mysqli_fetch_array($query)){
+                        echo "<tr>";
+                        echo "<td>".$siswa['id']."</td>";
+                        echo "<td>".$siswa['nama']."</td>";
+                        echo "<td>".$siswa['alamat']."</td>";
+                        echo "<td>".$siswa['jenis_kelamin']."</td>";
+                        echo "<td>".$siswa['agama']."</td>";
+                        echo "<td>".$siswa['sekolah_asal']."</td>";
+                        echo "</tr>";
+                    }
+                    ?>
 
-            echo "<td>".$siswa['id']."</td>";
-            echo "<td>".$siswa['nama']."</td>";
-            echo "<td>".$siswa['alamat']."</td>";
-            echo "<td>".$siswa['jenis_kelamin']."</td>";
-            echo "<td>".$siswa['agama']."</td>";
-            echo "<td>".$siswa['sekolah_asal']."</td>";
+                </tbody>
+                </table>
+                </center>
 
-            echo "</tr>";
-        }
-        ?>
+                <p>Total: <?php echo mysqli_num_rows($query) ?></p>
 
-    </tbody>
-    </table>
+            </div>
+        </div>
 
-    <p>Total: <?php echo mysqli_num_rows($query) ?></p>
+        <div id="footer">
+                
+                <div id="alamat">
+                    <p style="color: rgb(133, 133, 173); font-size: 15px;);">Alamat</p>
+                    <p style = "margin:0;">Jalan Sukun Raya 9, Besito, Gebog,</p>
+                    <p>Kudus, Jawa Tengah 59333</p>
+                </div>
+
+                <div id="kontak">
+                    <p style="color: rgb(133, 133, 173); font-size: 15px;" >Contact Person</p>
+            
+                </div> 
+
+
+                    
+                <div id="copyright">
+                    <p>Copyright © 2020, RPL RUS Web Dev</p>
+                </div>
+            </div>
 
     </body>
 </html>
